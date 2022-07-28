2540fc9ee618b48c5f4759b329f9f7169e05ade5
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-07-28 02:56:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://root:root@cluster0.ps3p8.mongodb.net/?retryWrites=true&w=majority'
   )
   .then(result => {
     User.findOne().then(user => {
