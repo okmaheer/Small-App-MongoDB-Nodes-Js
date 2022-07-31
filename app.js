27ec4aef5ba08b31d804d30870bcf2ccf40969d2
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-07-31 10:44:28 +0500

    depricaed error messade soluion

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(MONGODB_URI,{ useNewUrlParser: true,useUnifiedTopology: true })
   .then(result => {
     app.listen(3000);
   })
